293e5f9dfe408655c7b5419fd9761b0ccacf141c
Alex Yevtushenko <a@qx.zone>
2022-07-20 15:01:09 +0300

    Init commit

---
+++ mappings.js
@@ -0,0 +1,55 @@
+let state = {};
+
+const midiFloatValueConverter = max => x => x / 127.0 * max;
+const midiBoolValueConverter = x => !!x ? "1" : "0";
+
+const oscMapToFloatTarget = (path, conv) => ({
+  "oscPath": path,
+  "oscValueType": "f",
+  "valueConverter": x => {
+    if (!state[path]) state[path] = {};
+    state[path].current = x;
+    state[path].value = (conv || midiFloatValueConverter(state[path].max || 0.75))(x);
+    return state[path].value;
+  }
+});
+
+const oscMapToButtonTarget = path => ({
+  "oscPath": path,
+  "oscValueType": "i",
+  "valueConverter": midiBoolValueConverter
+});
+
+const oscMapToFloatFromPathMax = (path) => ({
+  "oscPath": path,
+  "oscValueType": "f",
+  "valueConverter": x => {
+    if (!state[path]) state[path] = {};
+    state[path].max = 0.75 + midiFloatValueConverter(0.25)(x);
+    return midiFloatValueConverter(state[path].max)(state[path].current || x);
+  }
+});
+
+module.exports = {
+  "setState": s => state = s,
+  "cc": {
+    "3":  oscMapToFloatTarget('/ch/01/mix/fader'),         // 1 - fader
+    "4":  oscMapToFloatTarget('/ch/03/mix/fader'),         // 2 - fader
+    "5":  oscMapToFloatTarget('/ch/09/mix/fader'),         // 3 - fader
+    "6":  oscMapToFloatTarget('/ch/11/mix/fader'),         // 4 - fader
+    "7":  oscMapToFloatTarget('/ch/13/mix/fader'),         // 5 - fader
+    "8":  oscMapToFloatTarget('/ch/15/mix/fader'),         // 6 - fader
+    "11": oscMapToFloatTarget('/lr/mix/fader'),            // 9 - fader
+
+    "23": oscMapToButtonTarget('/ch/01/mix/on'),           // 1 - on/off
+    "24": oscMapToButtonTarget('/ch/03/mix/on'),           // 2 - on/off
+    "25": oscMapToButtonTarget('/ch/09/mix/on'),           // 3 - on/off
+    "26": oscMapToButtonTarget('/ch/11/mix/on'),           // 4 - on/off
+    "27": oscMapToButtonTarget('/ch/13/mix/on'),           // 5 - on/off
+    "28": oscMapToButtonTarget('/ch/15/mix/on'),           // 6 - on/off
+    "31": oscMapToButtonTarget('/lr/mix/on'),              // 9 - on/off
+
+    "14": oscMapToFloatFromPathMax('/ch/01/mix/fader'),    // 1 - fader
+    "15": oscMapToFloatFromPathMax('/ch/03/mix/fader')     // 2 - fader
+  }
+};
---
+++ src/index.js
@@ -0,0 +1,193 @@
+const commandLineArgs = require('command-line-args');
+const easymidi = require('easymidi');
+const osc = require('osc');
+const fs = require('fs');
+const { exit } = require('process');
+const mqtt = require('mqtt');
+
+const optionDefinitions = [
+  { name: 'mappings', alias: 'm', type: String, defaultOption: true, defaultValue: '../mappings.js' },
+  { name: 'midi-list-devices', alias: 'l', type: Boolean },
+  { name: 'midi-device', alias: 'd', type: String, defaultValue: 'WORLDE easy CTRL' },
+  { name: 'xr18-address', alias: 'a', type: String, defaultValue: '10.9.9.215' },
+  { name: 'xr18-port', alias: 'p', type: Number, defaultValue: 10024 },
+  { name: 'mqtt-url', alias: 'b', type: String, defaultValue: 'tcp://10.9.9.96:1883' },
+  { name: 'mqtt-topic', alias: 't', type: String, defaultValue: 'dev/midi-to-xr18' }
+];
+const args = commandLineArgs(optionDefinitions);
+
+if (!!args['midi-list-devices']) {
+  console.log(easymidi.getInputs());
+  process.exit(0);
+}
+
+const mappings = require(args.mappings);
+
+const midiDeviceName = args['midi-device'];
+const xr18Addr = args['xr18-address'];
+const xr18Port = args['xr18-port'];
+
+const udpPort = new osc.UDPPort({
+  localAddress: "0.0.0.0",
+  localPort: 10023,
+  metadata: true
+});
+
+let pubSub;
+
+let midiDevice;
+try {
+  midiDevice = new easymidi.Input(midiDeviceName);
+}
+catch (error) {
+  console.error(error);
+  console.log('Available devices:');
+  console.log(easymidi.getInputs());
+  process.exit(-1);
+}
+
+let state = {};
+let stateSaveTimeout = null;
+function saveState(oscData, midiData) {
+  state[oscData.address] = { osc: oscData, midi: midiData };
+
+  if (!!stateSaveTimeout) clearTimeout(stateSaveTimeout);
+  stateSaveTimeout = setTimeout(function() {
+    stateSaveTimeout = null;
+    fs.writeFile('./state_data.json', JSON.stringify(state), (err) => !!err ? console.log("Error saving state to './stat_data.json': ", err) : 0);
+  }, 1000);
+}
+
+function loadData() {
+  fs.readFile('./state_data.json', function (err, data) {
+    if(!!err) {
+      console.log("Error saving state to './stat_data.json': ", err);
+      return;
+    }
+
+    console.log("Reading state from './stat_data.json");
+    state = JSON.parse(data);
+
+    mappings.setState(state);
+
+    const midiOutDevice = new easymidi.Output(midiDeviceName);
+    for (var i in state) {
+      console.log("Recover state for:", state[i]);
+      try {
+        midiOutDevice.send(state[i].midi._type, state[i].midi);
+      }
+      catch (e) {
+        console.log("Error sending MIDI command: ", e);
+      }
+    }
+  
+    midiOutDevice.close();
+  });
+}
+
+udpPort.open();
+udpPort.on('message', function (oscMsg, timeTag, info) {
+  console.log("An OSC message received: ", oscMsg);
+  console.log("Remote info is: ", info);
+});
+
+udpPort.on('ready', () => {
+  // udpPort.send({ address: '/info' }, xr18Addr, xr18Port);
+  // udpPort.send({ address: '/status' }, xr18Addr, xr18Port);
+  // udpPort.send({ address: '/xremote' }, xr18Addr, xr18Port);
+
+  loadData();
+});
+
+midiDevice.on('cc', msg => {
+  console.log(msg);
+  let oscMap = mappings[msg._type][msg.controller] || null;
+  if (oscMap == null) return;
+
+  let type = oscMap.oscValueType;
+  let val = oscMap.valueConverter(msg.value);
+  let rawVal = msg.value;
+  let addr = oscMap.oscPath;
+
+  var data = {
+    address: addr,
+    args: [{
+        type: type,
+        value: val
+    }]
+  };
+
+  console.log("Mapping found! Sending command to XR18: ", data);
+  try {
+    udpPort.send(data, xr18Addr, xr18Port);
+    saveState(data, msg);
+  }
+  catch (error) {
+    console.warn("Error sending command to XR18!", error);
+  }
+
+  try {
+    pubSub.publish(args["mqtt-topic"] + addr, JSON.stringify({
+      type: type,
+      value: val,
+      raw_value: rawVal
+    }));
+  }
+  catch (error) {
+    console.warn("Error publishing to MQTT!", error);
+  }
+});
+
+midiDevice.on('program', msg => console.log(msg));
+
+// MQTT
+pubSub = mqtt.connect(args['mqtt-url'], { clientId: "midi-to-xr18" });
+pubSub.on('connect', () => {
+  console.log('MQTT connected!');
+
+  for (var i in mappings.cc) {
+    let el = mappings.cc[i];
+    let topic = args["mqtt-topic"] + el.oscPath + "/set";
+
+    console.log('Subscribing to MQTT topic: "' + topic + '"');
+    pubSub.subscribe(topic, (e) => { if (e) console.warn("Failed to subscribe on MQTT topic: '" + topic + "'", e); });
+  }
+});
+
+pubSub.on('message', (t, m) => {
+  let ms = m.toString();
+  console.log('Received message from "' + t + '"', ms);
+
+  try {
+    let jm = JSON.parse(ms);
+
+    udpPort.send(jm, xr18Addr, xr18Port);
+    // saveState(data, msg);
+  }
+  catch (error) {
+    console.warn("Error sending command from MQTT to XR18!", error);
+  }
+});
+
+function cleanup() {
+  console.log("Cleanup before exit");
+  midiDevice.close();
+  udpPort.close();
+}
+
+function exitHandler(opts, exitCode) {
+    if (opts.cleanup) cleanup();
+    if (opts.exit) process.exit(exitCode || 0);
+}
+
+process.on('exit', exitHandler.bind(null,{cleanup:true}));
+
+//catches ctrl+c event
+process.on('SIGINT', exitHandler.bind(null, {exit:true}));
+
+process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
+process.on('SIGUSR2', exitHandler.bind(null, {exit:true}));
+
+process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
+
+process.stdin.resume();
